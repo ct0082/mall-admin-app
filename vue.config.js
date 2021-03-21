25ef48b8d22ef9b64bba6e9c6444432cba480fe4
ct0082 <2238848298@qq.com>
2021-03-21 15:04:55 +0800

    系统初始布局

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports={
+    css:{
+        loaderOptions:{
+            less:{
+                javascriptEnabled:true,
+            }
+        }
+    },
+    lintOnSave: false,
+
+    devServer: {
+
+        overlay: {
+
+            warning: false,
+
+            errors: false
+
+        }
+
+    },
+}

f76f47fb28ed9613376ebb110c42abae1f8109d8
ct0082 <2238848298@qq.com>
2021-03-24 23:00:09 +0800

    搜索组件实现

--- src/views/page/productList.vue
+++ src/views/page/productList.vue
@@ -1,3 +1,19 @@
 <template>
-    <div>商品列表</div>
+    <div class="product-list">
+        <search-box @submit="searchSubmit"/>
+    </div>
 </template>
+
+<script>
+import searchBox from "@/components/search.vue";
+export default {
+    components:{
+        searchBox,
+    },
+    methods:{
+        searchSubmit(form){
+            console.log(form);
+        }
+    }
+}
+</script>
